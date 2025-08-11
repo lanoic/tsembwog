9af90c179299074e7158d2e1a83774be2eca167b
lanoic <31804308+lanoic@users.noreply.github.com>
2025-08-11 19:22:46 -0400

    fix: indentation error in seed.py

--- backend/seed.py
+++ backend/seed.py
@@ -20,5 +20,5 @@ def run():
 if __name__=="__main__": run()
     # demo api key
     if not db.query(APIKey).first():
-        db.add(APIKey(key='DEMO-KEY', label='demo', owner_user_id=admin.id));
+        db.add(APIKey(key='DEMO-KEY', label='demo', owner_user_id=admin.id))
         db.commit()
